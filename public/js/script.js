ee1c66bd04d4a7de3fff55d1a2e0be3582c9800c
codintag <codintag@gmail.com>
2020-09-01 08:47:09 +0100

    some changes added, button appear when scroll on mobile and desktop

---
+++ public/js/script.js
@@ -0,0 +1,35 @@
+/* Js begin here */
+
+let mobNav = document.getElementById('nav');
+let navLogo = document.querySelector('.navLogo');
+let sinscrire = document.querySelector('.inscrire');
+let liwrapper = document.querySelector('.liwrapper');
+
+this.addEventListener('scroll', () => {
+    if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+        mobNav.style.top = "0";
+        mobNav.style.backgroundColor = '#040714' //'rgba(0, 0, 0, .8)';
+        // mobNav.style.transition= 'all 1s'
+        // navLogo.style.transition= 'all 1s'
+        navLogo.style.display = 'initial';
+        sinscrire.style.display = 'initial';
+        liwrapper.style.display = 'initial';
+
+
+    } else {
+        mobNav.style.top = "0";
+        mobNav.style.backgroundColor = '';
+        navLogo.style.display = 'none';
+        sinscrire.style.display = 'none';
+        liwrapper.style.display = 'none';
+
+
+    }
+});
+
+this.addEventListener('scroll', () => {
+    if (window.innerHeight + window.scrollY > document.body.clientHeight) {
+        liwrapper.style.display = 'none';
+        sinscrire.style.display = 'none';
+    }
+});
